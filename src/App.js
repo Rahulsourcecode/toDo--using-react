c4f4796fd7eab45fe32b3104e69e5e24c47e3800
Rahul R <rajeshrahul831@gmail.com>
2023-05-03 16:12:05 +0530

    sec

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+  let day = daylist[new Date().getDay()]
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
+
+  const handleDelete = (id) => {
+    setToDos(toDos.filter((todo) => todo.id !== id));
+  };
+
+  const handleCheck = (id) => {
+    setToDos(
+      toDos.map((todo) => {
+        if (todo.id === id) {
+          todo.status = !todo.status;
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {day} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+     <i
+  onClick={() => {
+    setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+    setToDo('');
+  }}
+  className="fas fa-plus"
+></i>
+
+      </div>
+      <div className="todos">
+        {toDos.map((todo) => (
+          <div className="todo">
+            <div className="left">
+              <input
+                onChange={() => handleCheck(todo.id)}
+                checked={todo.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{todo.text}</p>
+            </div>
+            <div className="right">
+              <i
+                onClick={() => handleDelete(todo.id)}
+                className="fas fa-times"
+              ></i>
+            </div>
+          </div>
+        ))}        
+        <h1>Completed Task's</h1>     
+        {toDos
+          .filter((todo) => todo.status)
+          .map((todo) => (
+            <div className="todo">
+              <div className="left">
+                <input
+                  onChange={() => handleCheck(todo.id)}
+                  checked={todo.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{todo.text}</p>
+              </div>
+              <div className="right">
+                <i
+                  onClick={() => handleDelete(todo.id)}
+                  className="fas fa-times"
+                ></i>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
